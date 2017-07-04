c536fcc09dd07b37d932c39b5a44f478e68b60d1
lipeng <lipeng21@baidu.com>
2017-07-04 19:47:40 +0800

    给GridLayoutManager和StaggeredGridLayoutManager 增加了footer功能

--- BaseLibrary/src/main/java/io/github/trylovecatch/baselibrary/list/footer/FooterAdapter.java
+++ BaseLibrary/src/main/java/io/github/trylovecatch/baselibrary/list/footer/FooterAdapter.java
@@ -1,6 +1,9 @@
 package io.github.trylovecatch.baselibrary.list.footer;
 
 import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.ViewGroup;
 import io.github.trylovecatch.baselibrary.list.BaseAdapter;
 import io.github.trylovecatch.baselibrary.list.BaseViewHolder;
@@ -52,6 +55,40 @@ public class FooterAdapter extends BaseAdapter {
             return tCount + 1;
         }
         return tCount;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+
+        if (layoutManager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = ((GridLayoutManager) layoutManager);
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    if (getItemViewType(position) == TYPE_FOOTER) {
+                        return gridManager.getSpanCount();
+                    } else {
+                        return 1;
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onViewAttachedToWindow(BaseViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+        if( lp != null && lp instanceof StaggeredGridLayoutManager.LayoutParams ) {
+            StaggeredGridLayoutManager.LayoutParams  params =(StaggeredGridLayoutManager.LayoutParams) lp;
+            if(holder.getItemViewType()==TYPE_FOOTER){
+                params.setFullSpan(true);
+            }else{
+                params.setFullSpan(false);
+            }
+        }
     }
 
     public void setIsHasMore(boolean pIsHasMore) {
